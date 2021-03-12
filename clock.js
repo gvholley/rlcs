b8913f3b6773c37f540a59ec6ff36274a145416a
Grant Holley <gvholley@gmail.com>
2021-03-12 20:22:52 +0900

    Added JS clock

---
+++ clock.js
@@ -0,0 +1,19 @@
+//Clock logic
+
+
+function mytime()
+   {
+   var d=new Date();
+   ap="AM";
+   h=d.getHours();
+   m=d.getMinutes();
+   s=d.getSeconds();
+   if (h>11) { ap = "PM"; }
+   if (h>12) { h = h-12; }
+   if (h==0) { h = 12; }
+   if (m<10) { m = "0" + m; }
+   if (s<10) { s = "0" + s; }
+   document.getElementById('timehere').innerHTML=h + ":" + m + ":" + s + " " + ap;
+   t=setTimeout('mytime()',500);
+   }
+
